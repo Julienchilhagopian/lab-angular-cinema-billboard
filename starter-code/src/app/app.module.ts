1724f2fde778593a2bb0e27ea6b5efb0374257bf
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-29 15:11:24 +0200

    ite3

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -1,20 +1,40 @@
+// ---- Modules ---- //
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
+// ---- Components ---- //
 import { AppComponent } from './app.component';
+import { MyMovieComponent } from './components/my-movie/my-movie.component';
+import { MyHomeComponent } from './components/my-home/my-home.component';
+
+// ---- Services ---- //
+import { MoviesService } from './services/movies.service';
+
+// ---- Routes ---- //
+const routes: Routes = [
+  { path: 'home', component: MyHomeComponent },
+  { path: 'movie/:id', component: MyMovieComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyMovieComponent,
+    MyHomeComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    MoviesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
